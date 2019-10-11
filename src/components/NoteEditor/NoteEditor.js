9ee17216c19d58a6dad023ace75f538cad5827ec
Elena Pavlova <lenlho22@gmail.com>
2019-10-11 21:19:19 +0300

    Add notes from NoteEditor

--- src/components/NoteEditor/NoteEditor.js
+++ src/components/NoteEditor/NoteEditor.js
@@ -1,10 +1,66 @@
 import React, { Component } from 'react'
+import './NoteEditor.scss'
 
 class NoteEditor extends Component {
+  state = {
+    text: '',
+  }
+
+  editInput = React.createRef()
+
+  handleTextChange = e => {
+    this.setState({ text: e.target.value })
+  }
+
+  handleKeyDown = e => {
+    if (e.keyCode === 27) {
+      this.setState({ text: '' })
+      return
+    }
+
+    if (e.keyCode === 13) {
+      this.handleNoteAdd()
+    }
+  }
+
+  handleNoteAdd = () => {
+    if (this.state.text.trim() === '') {
+      this.setState({ text: '' })
+      return
+    }
+
+    const newNote = {
+      text: this.state.text,
+      isDone: false,
+      id: Date.now(),
+    }
+
+    this.props.onNoteAdd(newNote)
+    this.setState({ text: '' })
+    this.editInput.current.focus()
+  }
+
+  componentDidMount() {
+    this.editInput.current.focus()
+  }
+
   render() {
+    const { text } = this.state
+
     return (
-      <div>
-        <h2>NoteEditor</h2>
+      <div className="note-editor">
+        <input
+          type="text"
+          placeholder="Enter your new task here..."
+          className="note-editor__textarea"
+          ref={this.editInput}
+          value={text}
+          onChange={this.handleTextChange}
+          onKeyDown={this.handleKeyDown}
+        />
+        <button className="add-button" onClick={this.handleNoteAdd}>
+          Add
+        </button>
       </div>
     )
   }
--- src/components/ToDoApp/ToDoApp.js
+++ src/components/ToDoApp/ToDoApp.js
@@ -4,25 +4,52 @@ import NotesList from '../NotesList'
 import FiltersList from '../FiltersList'
 import './ToDoApp.scss'
 
-const initNotes = [
-  { text: '2233', isDone: false, id: 1570801672949 },
-  { text: 'sdsffd', isDone: true, id: 1570801664082 },
-]
-
 class ToDoApp extends Component {
   state = {
     notes: [],
     displayedNotes: [],
   }
 
+  handleNoteAdd = newNote => {
+    const { notes } = this.state
+    const newNotes = [newNote, ...notes]
+    // const newFilters = this.state.filters.slice();
+    // this.setState({ notes: newNotes, displayedNotes: newNotes}, () => {
+    //     const curFilter = newFilters.filter(item => item.isActive === true);
+    //     if (curFilter[0].name === 'done') {
+    //         this.handleNoteFilter('new');
+    //     } else {
+    //         this.handleNoteFilter(curFilter[0].name);
+    //     }
+    // });
+    this.setState({ notes: newNotes, displayedNotes: newNotes })
+  }
+
+  _updateLocalStorage = () => {
+    const notes = JSON.stringify(this.state.notes)
+    localStorage.setItem('todoNotes', notes)
+  }
+
   componentDidMount() {
-    this.setState({
-      notes: initNotes,
-      displayedNotes: initNotes,
-    })
+    const localNotes = localStorage.getItem('todoNotes')
+
+    if (localNotes) {
+      const initNotes = JSON.parse(localNotes)
+      this.setState({
+        notes: initNotes,
+        displayedNotes: initNotes,
+      })
+    }
+  }
+
+  componentDidUpdate() {
+    console.log('%c%s', 'color: blue', 'componentDidUpdate!')
+    this._updateLocalStorage()
   }
 
   render() {
+    console.log('%c%s', 'color: red', 'render!')
+
     const { displayedNotes } = this.state
 
     return (
@@ -30,7 +57,7 @@ class ToDoApp extends Component {
         <header className="todo-app__header">
           <h1 className="todo-app__title">ToDo App</h1>
         </header>
-        <NoteEditor />
+        <NoteEditor onNoteAdd={this.handleNoteAdd} />
         <NotesList notes={displayedNotes} />
         <FiltersList />
       </div>
